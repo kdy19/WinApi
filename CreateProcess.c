30663688a92353acfd943ba0a2200bc8bdfb73f3
kdy19 <qweasd0717@icloud.com>
2021-07-03 11:45:27 +0000

    CreateProcess

--- CreateProcess.c
+++ CreateProcess.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <windows.h>
-#include <stdbool.h>
+
+/*
+typedef struct _STARTUPINFOA {
+  DWORD  cb;
+  LPSTR  lpReserved;
+  LPSTR  lpDesktop;
+  LPSTR  lpTitle;
+  DWORD  dwX;
+  DWORD  dwY;
+  DWORD  dwXSize;
+  DWORD  dwYSize;
+  DWORD  dwXCountChars;
+  DWORD  dwYCountChars;
+  DWORD  dwFillAttribute;
+  DWORD  dwFlags;
+  WORD   wShowWindow;
+  WORD   cbReserved2;
+  LPBYTE lpReserved2;
+  HANDLE hStdInput;
+  HANDLE hStdOutput;
+  HANDLE hStdError;
+} STARTUPINFOA, *LPSTARTUPINFOA;
+
+typedef struct _PROCESS_INFORMATION {
+  HANDLE hProcess;
+  HANDLE hThread;
+  DWORD  dwProcessId;
+  DWORD  dwThreadId;
+} PROCESS_INFORMATION, *PPROCESS_INFORMATION, *LPPROCESS_INFORMATION;
+*/
 
 int main(int argc, char* argv[]) 
 {
@@ -12,6 +41,8 @@ int main(int argc, char* argv[])
 	memset(&pi, 0, sizeof(pi));
 
 	CreateProcess(L"C:\\WINDOWS\\system32\\calc.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	printf("%d\n", pi.dwProcessId);
+	printf("%d\n", GetProcessId(pi.hProcess));
 
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
